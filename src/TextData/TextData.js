4d530d70ea84529fd153dce1043a501ce1b0ff88
Matthew Trontz <mtrontz@trontzenterprises.com>
2021-10-07 12:33:38 -0400

    Update: User Profile Info

--- src/TextData/TextData.js
+++ src/TextData/TextData.js
@@ -1,54 +1,48 @@
 
 
-export const profile_contacts_arr =  [
-        {
-          id: "li",
-          name: "LinkedIn",
-          link: "https://www.linkedin.com/in/datta-arnab/",
-          iconlink: "linkedin",
-          color: "#007fc4",
-        },
-        {
-          id: "gh",
-          name: "GitHub",
-          link: "https://github.com/arnab-datta",
-          iconlink: "github",
-          color: "#0e0e0e",
-        },
-        {
-          id: "so",
-          name: "Stack Overflow",
-          link: "https://stackoverflow.com/story/arnab-datta",
-          iconlink: "stackoverflow",
-          color: "#ffb24a",
-        },
-        {
-          id: "em",
-          name: "Email",
-          link: "mailto:dattaarnab2013@gmail.com",
-          iconlink: "email",
-          color: "#0171b3",
-        },
-        {
-          id: "fb",
-          name: "Facebook",
-          link: "https://www.facebook.com/arnab.datta.babai/",
-          iconlink: "facebook",
-          color: "#3b5a9a",
-        },
-        {
-          id: "ins",
-          name: "Instagram",
-          link: "https://www.instagram.com/urs_arnab_/",
-          iconlink: "instagram",
-          color:
-            "linear-gradient(45deg, #405de6,#5851db, #833ab4, #c13584, #e1306c, #fd1d1d)",
-        },
-        {
-          id: "tw",
-          name: "Twitter",
-          link: "https://twitter.com/ArnabDatta8",
-          iconlink: "twitter",
-          color: "#60a9dc",
-        },
-      ];
+export const profile_contacts_arr =  [ 
+  {
+    id: "gh",
+    name: "GitHub",
+    link: "https://github.com/mtrontz-the-dev",
+    iconlink: github,
+    color: "#0e0e0e",
+  },
+
+  {
+    id: "so",
+    name: "Stack Overflow",
+    link: "https://stackoverflow.com/users/15817172/mtrontz-the-dev",
+    iconlink: stackoverflow,
+    color: "#ffb24a",
+  },
+  {
+    id: "em",
+    name: "Email",
+    link: "mailto:mtrontz@trontzenterprises.com",
+    iconlink: email,
+    color: "linear-gradient(45deg, #d5135a, #f05924)",
+  },
+  {
+    id: "fb",
+    name: "Facebook",
+    link: "https://www.facebook.com/matthew.trontz",
+    iconlink: facebook,
+    color: "#3b5a9a",
+  },
+  {
+    id: "ins",
+    name: "Instagram",
+    link: "https://www.instagram.com/mtrontz_the_dev",
+    iconlink: instagram,
+    color:
+      "linear-gradient(45deg, #405de6,#5851db, #833ab4, #c13584, #e1306c, #fd1d1d)",
+  },
+  {
+    id: "tw",
+    name: "Twitter",
+    link: "https://twitter.com/mtrontz_the_dev",
+    iconlink: twitter,
+    color: "#60a9dc",
+  },
+];
